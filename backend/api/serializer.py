719806a8d7744d7af2787d4481aba1806775c862
Aneeb Paloth <aneebpaloth@Aneebs-MacBook-Air.local>
2024-07-08 10:29:16 +0530

    added serializer class for all models

--- backend/api/serializer.py
+++ backend/api/serializer.py
@@ -47,7 +47,90 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = '__all__'
 
+
+class PasswordResetSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+
+
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = '__all__'
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    post_count = serializers.SerializerMethodField()
+
+    def get_post_count(self, category):
+        return category.posts.count()
+
+    class Meta:
+        model = Category
+        fields = [
+            "id","title","image","slug","post_count",
+        ]
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta: 
+        model = Comment
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super(CommentSerializer,self).__init__( *args, **kwargs)
+        request = self.context.get('request')
+        if request and request.method == 'POST':
+            self.Meta.depth = 0
+        else:
+            self.Meta.depth = 1
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta: 
+        model = Post
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super(PostSerializer,self).__init__( *args, **kwargs)
+        request = self.context.get('request')
+        if request and request.method == 'POST':
+            self.Meta.depth = 0
+        else:
+            self.Meta.depth = 1
+
+
+class BookmarkSerializer(serializers.ModelSerializer):
+    class Meta: 
+        model = Bookmark
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super(BookmarkSerializer,self).__init__( *args, **kwargs)
+        request = self.context.get('request')
+        if request and request.method == 'POST':
+            self.Meta.depth = 0
+        else:
+            self.Meta.depth = 1
+
+
+class NotificationSerializer(serializers.ModelSerializer):
+    class Meta: 
+        model = Notification
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super(NotificationSerializer,self).__init__( *args, **kwargs)
+        request = self.context.get('request')
+        if request and request.method == 'POST':
+            self.Meta.depth = 0
+        else:
+            self.Meta.depth = 1
+
+
+class AuthorSerializer(serializers.Serializer):
+    views = serializers.IntegerField(default=0)
+    posts = serializers.IntegerField(default=0)
+    likes = serializers.IntegerField(default=0)
+    bookmarks = serializers.IntegerField(default=0)
+
+    
